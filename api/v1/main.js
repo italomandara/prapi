faeb4ef8e45a191837717bef805731ec2a1b41ea
Italo Mandara <italomandara@gmail.com>
2025-12-22 19:05:58 +0100

    don't limit

--- api/v1/main.js
+++ api/v1/main.js
@@ -1,15 +1,15 @@
 import { requireApiKey } from "../../lib/auth";
 
 export default async function handler(req, res) {
-  const { success } = await ratelimit.limit(req.headers["x-api-key"]);
+  // const { success } = await ratelimit.limit(req.headers["x-api-key"]);
 
-  if (!success) {
-    return res.status(429).json({ error: "Too many requests" });
-  }
+  // if (!success) {
+  //   return res.status(429).json({ error: "Too many requests" });
+  // }
 
-  if (!requireApiKey(req)) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+  // if (!requireApiKey(req)) {
+  //   return res.status(401).json({ error: "Unauthorized" });
+  // }
 
   res.json({ data: "Public API response" });
 }
