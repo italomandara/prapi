e7980b53f2ad071fcd35a239c98c1d1ab54fee0a
Italo Mandara <italomandara@gmail.com>
2026-02-11 18:27:29 +0100

    add proxy

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@ export const CACHE_LIFESPAN = "86000";
 export const CACHE_STALE_REVALIDATE = "86000";
 export const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "prapi-1hucjdrod-italos-projects-d922b910.vercel.app";
+  : "https://prapi-chi.vercel.app";
