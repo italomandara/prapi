55460714989bbb51c25e218eda69822c145571f4
Italo Mandara <italomandara@gmail.com>
2026-03-02 11:49:45 +0100

    rename endpoints

--- api/v1/profile/index.ts
+++ api/v1/profile/index.ts
@@ -27,6 +27,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   });
 }
 
+// http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&steamid=76561197960435530&relationship=friend
+// http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&steamid=76561197972495328
+// http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json
+
 // import axios from "axios";
 // import type { VercelRequest, VercelResponse } from "@vercel/node";
 // import { requireApiKey } from "../../../lib/auth.js";
