dedb9b991898dd60a4da92255524b7cf895dc5ef
Italo Mandara <italomandara@gmail.com>
2026-05-13 22:09:04 +0200

    fix customgame endpoint method

--- api/v1/custom/index.ts
+++ api/v1/custom/index.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method !== "GET") {
+  if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
